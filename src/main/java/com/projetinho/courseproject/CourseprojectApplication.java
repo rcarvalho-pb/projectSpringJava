559b1eaef30728a994995573643ac5d1fb7f006a
Ramon Carvalho <rcarvalho.pb@gmail.com>
2022-05-14 10:29:56 -0300

    Project created

---
+++ src/main/java/com/projetinho/courseproject/CourseprojectApplication.java
@@ -0,0 +1,13 @@
+package com.projetinho.courseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetinho/courseproject/CourseprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetinho.courseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
